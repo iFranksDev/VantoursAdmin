12de81770549468dbfcbf53440f3832484073a63
franks <franksdev@frankss-MacBook.local>
2020-08-16 13:59:12 -0500

    Final

--- src/js/login.js
+++ src/js/login.js
@@ -8,40 +8,6 @@ M.AutoInit();
 
 ///////////////////////////////////////////Materialize///////////////////////////////////////////
 
-//////////////////////////////////////////Borrar Version Final///////////////////////////////////
-(function(){
-
-    var ui = new firebaseui.auth.AuthUI(firebase.auth());
-
-
-   var uiConfig = {
-        callbacks: {
-          signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-            return true;
-          },
-          uiShown: function() {
-            document.getElementById('loader').style.display = 'none';
-          }
-        },
-        signInFlow: 'popup',
-        signInSuccessUrl: '../views/main.html',
-        signInOptions: [
-          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-          firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-          firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-          firebase.auth.GithubAuthProvider.PROVIDER_ID,
-          firebase.auth.EmailAuthProvider.PROVIDER_ID,
-          firebase.auth.PhoneAuthProvider.PROVIDER_ID
-        ],
-        tosUrl: '<your-tos-url>',
-        privacyPolicyUrl: '<your-privacy-policy-url>'
-      };
-
-      ui.start('#firebaseui-auth-container', uiConfig);
-
-})()
-//////////////////////////////////////////Borrar Version Final///////////////////////////////////
-
 
 
 /*
